fe52bd59dc9f8888d60844a39e1e0d2064fc3690
Abbeyha <aeh4659@gmail.com>
2026-09-15 18:27:01 -0500

    Question: 4

--- assignment1/docker_commands.sh
+++ assignment1/docker_commands.sh
@@ -14,3 +14,6 @@ docker run -d --name layered_ex2 logistic_regression_layered:latest
 docker exec -it layered_ex2 /bin/sh
 docker build -t logistic_regression_layered:latest .
 docker run --volume "${PWD}\data:/home/iem4723/data" logistic_regression_layered:latest
+# Question 4
+docker build -t simple-file-server .
+docker run --name simple_file_server --publish 3333:8000 -d simple-file-server:latest
