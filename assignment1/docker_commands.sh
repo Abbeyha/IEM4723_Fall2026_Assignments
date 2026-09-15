01db5d952267cb1284c5b487813988b3efeb2a7c
Abbeyha <aeh4659@gmail.com>
2026-09-15 16:48:46 -0500

    Question 2: Volume Mounting

--- assignment1/docker_commands.sh
+++ assignment1/docker_commands.sh
@@ -4,3 +4,6 @@ docker tag linear-regression-app:latest linear-regression-app:v0.1.1
 docker tag linear-regression-app:latest linear-regression-app:v0.1.2
 docker tag linear-regression-app:latest abbeygayle/linear-regression-app:latest
 docker push abbeygayle/linear-regression-app:latest
+# Question 2
+docker build -t regression-volume-app .
+docker run --volume "${PWD}\data:/home/iem4723/data" regression-volume-app
